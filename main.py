894df5832a96f0e288abbe6adf391264843001bb
kamilksiazek <kamil.802@interia.pl>
2022-10-22 14:34:22 +0200

    Several comparisons, initial version

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import numpy as np
 from utils.paths import get_subjects
 from utils.graphs import GraphFromCSV, delete_rois
 from multiprocessing import cpu_count, Process
-from tqdm import tqdm
 
 def create_tensor_from_multiple_adjacency_graphs(paths, prefix_name):
     adjacencies = []
@@ -34,18 +33,38 @@ if __name__ == '__main__':
     stroke_acute_paths = [path for path in files if '/Stroke/ses-acute/' in path]
     stroke_control_paths = [path for path in files if '/Stroke/ses-control' in path]
     stroke_followup_paths = [path for path in files if '/Stroke/ses-followup' in path]
-    stroke_followup2_paths = [path for path in files if '/Stroke/ses-followup2' in path]     # followup2 is after 1 year
-    
+    # stroke_followup2_paths = [path for path in files if '/Stroke/ses-followup2' in path]     # followup2 is after 1 year
+    glioma_preop_paths = [path for path in files if '/Glioma/ses-preop/' in path]
+    glioma_postop_paths = [path for path in files if '/Glioma/ses-postop/' in path]
+    glioma_control_paths = [path for path in files if '/Glioma/ses-control/' in path]
+
     stroke_acute_adj = create_tensor_from_multiple_adjacency_graphs(stroke_acute_paths, 'stroke_acute')
     stroke_control_adj = create_tensor_from_multiple_adjacency_graphs(stroke_control_paths, 'stroke_control')
     stroke_followup_adj = create_tensor_from_multiple_adjacency_graphs(stroke_followup_paths, 'stroke_followup')
-    stroke_followup2_adj = create_tensor_from_multiple_adjacency_graphs(stroke_followup2_paths, 'stroke_followup2')
 
+    glioma_preop_adj = create_tensor_from_multiple_adjacency_graphs(glioma_preop_paths, 'glioma_preop')
+    glioma_postop_adj = create_tensor_from_multiple_adjacency_graphs
+    (glioma_postop_paths, 'glioma_postop')
+    glioma_control_adj = create_tensor_from_multiple_adjacency_graphs
+    (glioma_control_paths, 'glioma_control')
+
+    # stroke_followup2_adj = create_tensor_from_multiple_adjacency_graphs(stroke_followup2_paths, 'stroke_followup2')
 
     procs = []
 
-    procs.append(Process(target=run, args=(stroke_acute_adj, stroke_control_adj, 'stoke_acute vs stroke_control')))
-    procs[-1].start()
+    experiments = [[stroke_acute_adj, stroke_control_adj, 'stoke_acute vs stroke_control'],
+                   [glioma_preop_adj, glioma_control_adj, 'glioma preop vs glioma control'],
+                   [stroke_acute_adj, glioma_preop_adj, 'stroke acute vs glioma preop'],
+                   [glioma_preop_adj, glioma_postop_adj, 'glioma preop vs glioma postop'],
+                   [glioma_postop_adj, glioma_control_adj, 'glioma postop vs glioma control'],
+                   [stroke_followup_adj, stroke_control_adj, 'stroke followup vs stroke control'],
+                   [stroke_followup_adj, stroke_acute_adj, 'stroke followup vs stroke acute']]
+    for exp in experiments:
+        procs.append(Process(target=run, args=(exp[0], exp[1], exp[2])))
+        procs[-1].start()
+
+    # procs.append(Process(target=run, args=(stroke_acute_adj, stroke_control_adj, 'stoke_acute vs stroke_control')))
+    # procs[-1].start()
     # Do it for all combinations we want to test
 
     for p in procs:

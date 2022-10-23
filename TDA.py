b62475e8b16c1d878cb3bdf3f6c1b5f1e167c2fd
JoanSano <j.roget@sanoscience.org>
2022-10-23 14:19:35 +0200

    TDA final implementation

--- TDA.py
+++ TDA.py
@@ -1,10 +1,12 @@
 import networkx as nx
-import nxviz
+#import nxviz
 import gudhi
 import matplotlib.pyplot as plt 
 import numpy as np
 import seaborn as sns
 import pandas as pd
+from threading import Thread, Lock
+from time import sleep
 
 from utils.paths import get_subjects
 from TDAlib import *
@@ -14,62 +16,119 @@ plt.rcParams.update({
     "text.usetex": False
 })
 lineList = [line.rstrip('\n') for line in open('ROIs.txt')]
-
+lock = Lock()
 # Start with the curvature of each patient, 
 # then make a distribution for each category and compare across timepoints
 
+class Test(Thread):
+    def __init__(self, matrix, name, persistences, log):
+        Thread.__init__(self)
+        self.matrix = matrix
+        self.name = name
+        self.persistences = persistences
+        self.log = log
+
+    def run(self):
+        if self.log:
+            self.matrix = np.log1p(self.matrix)
+        self.matrix/= np.max(self.matrix)
+        subj_name = self.name.split('_')[0]
+        #os.makedirs(f'Plots/TDA/{subj_name}/', exist_ok=True)
+
+        np.fill_diagonal(self.matrix, 0)
+        Pdmatrix = pd.DataFrame(self.matrix)
+        Pdmatrix.columns = lineList
+        Pdmatrix.index = lineList
+        Pdmatrix = Pdmatrix.sort_index(0).sort_index(1)
+        mask = np.zeros_like(Pdmatrix.values, dtype=bool) 
+        mask[np.triu_indices_from(mask)] = True 
+
+        # This command will show you the functional connectivity matrix with Seaborn
+        '''
+        plt.figure(figsize = (20, 20))
+        _ = sns.heatmap(np.log1p(self.matrix), cmap='coolwarm', cbar=True, square=False, mask=mask)
+        plt.savefig(f'Plots/TDA/{subj_name}/{self.name}_masked.png')
+        plt.close()
+        '''
+        # transform the connectivity matrix in a distance matrix
+        self.matrix = np.ones_like(self.matrix) - self.matrix 
+        rips_complex = gudhi.RipsComplex(distance_matrix=self.matrix)
+        simplex_tree = rips_complex.create_simplex_tree(max_dimension=3)
+        diag = simplex_tree.persistence()
+
+        lock.acquire()
+        self.persistences.extend(diag)
+        lock.release()
+        """ gudhi.plot_persistence_barcode(diag, legend=False, max_intervals=0)
+        plt.savefig(f'Plots/TDA/{subj_name}/{name}_persistence_barcode.png')
+        plt.close()
+
+        gudhi.plot_persistence_diagram(diag, legend=True, max_intervals=0)
+        plt.tick_params(axis='both', labelsize=15)
+        plt.xlabel('Birth', fontsize=15)
+        plt.ylabel('Death', fontsize=15)
+        plt.savefig(f'Plots/TDA/{subj_name}/{name}_persistence_diagram.png')
+        plt.close()
+
+        gudhi.plot_persistence_density(diag, dimension=1)
+        plt.savefig(f'Plots/TDA/{subj_name}/{name}_persistence_density.png')
+        plt.close() """
+
+        '''
+        plotEuler_thr(matrix, 70)
+        plt.savefig(f'Plots/TDA/{name}_euler_entrophy.png')
+        plt.close()
+
+        curvalues = Curv_thr(i=matrix, e=0.7)
+        dict(zip(lineList[:10], curvalues[:10]))
+        plt.figure(figsize=(20,5))
+        sns.distplot(curvalues, kde=True, norm_hist=False)
+        plt.xlabel('Curvature values')
+        plt.ylabel('Counts')
+        plt.savefig(f'Plots/TDA/{name}_curvature.png')
+        plt.close()
+        '''
 
 
-def test(matrix, name):
-    matrix/= np.max(matrix)
-    subj_name = name.split('_')[0]
-    os.makedirs(f'Plots/TDA/{subj_name}/', exist_ok=True)
+def perform_analysis(population, population_name, log, threads=4):
+    for subj in population:
+        procs.append(Test(drop_data_in_connect_matrix(load_matrix(subj)), subj.split(os.sep)[-1].split('.')[0], persistences, log))
+        procs[-1].start()
+        while len(procs)>=threads:
+            for p in procs:
+                p.join(2)
+                if not p.is_alive():
+                    procs.remove(p)
+                    break
 
-    np.fill_diagonal(matrix, 0)
-    Pdmatrix = pd.DataFrame(matrix)
-    Pdmatrix.columns = lineList
-    Pdmatrix.index = lineList
-    Pdmatrix = Pdmatrix.sort_index(0).sort_index(1)
-    mask = np.zeros_like(Pdmatrix.values, dtype=np.bool) 
-    mask[np.triu_indices_from(mask)] = True 
+    for p in procs:
+        p.join()
 
-    # This command will show you the functional connectivity matrix with Seaborn
-    plt.figure(figsize = (20, 20))
-    _ = sns.heatmap(np.log1p(matrix), cmap='coolwarm', cbar=True, square=False, mask=mask)
-    plt.savefig(f'Plots/TDA/{subj_name}/{name}_masked.png')
+    type = 'log1p' if log else 'original'
+
+    plt.figure(figsize=(20,15))
+    gudhi.plot_persistence_barcode(persistences, legend=False, max_intervals=0)
+    plt.savefig(f'Plots/TDA/{population_name}_persistence_barcode_{type}.png')
     plt.close()
 
-    # transform the connectivity matrix in a distance matrix
-    matrix = np.ones_like(matrix) * np.max(matrix) - matrix 
-    rips_complex = gudhi.RipsComplex(distance_matrix=matrix)
-    simplex_tree = rips_complex.create_simplex_tree(max_dimension=2)
-    diag = simplex_tree.persistence()
-    gudhi.plot_persistence_barcode(diag, legend=True, max_intervals=0)
-    plt.savefig(f'Plots/TDA/{subj_name}/{name}_persistence_barcode.png')
-    plt.close()
-
-    gudhi.plot_persistence_diagram(diag, legend=True, max_intervals=0)
+    plt.figure(figsize=(35,20))
+    gudhi.plot_persistence_diagram(persistences, legend=True, max_intervals=0)
     plt.tick_params(axis='both', labelsize=15)
     plt.xlabel('Birth', fontsize=15)
     plt.ylabel('Death', fontsize=15)
-    plt.savefig(f'Plots/TDA/{subj_name}/{name}_persistence_diagram.png')
+    plt.savefig(f'Plots/TDA/{population_name}_persistence_diagram_{type}.png')
     plt.close()
 
-    gudhi.plot_persistence_density(diag, dimension=1)
-    plt.savefig(f'Plots/TDA/{subj_name}/{name}_persistence_density.png')
+    plt.figure(figsize=(20,15))
+    gudhi.plot_persistence_density(persistences, dimension=1)
+    plt.xlim([0,1]), plt.ylim([0,1])
+    plt.savefig(f'Plots/TDA/{population_name}_persistence_density_dim-1_{type}.png')
     plt.close()
 
-    plotEuler_thr(matrix, 70)
-    plt.savefig(f'Plots/TDA/{name}_euler_entrophy.png')
-    plt.close()
-
-    curvalues = Curv_thr(i=matrix, e=0.7)
-    dict(zip(lineList[:10], curvalues[:10]))
-    plt.figure(figsize=(20,5))
-    sns.distplot(curvalues, kde=True, norm_hist=False)
-    plt.xlabel('Curvature values')
-    plt.ylabel('Counts')
-    plt.savefig(f'Plots/TDA/{name}_curvature.png')
+    plt.figure(figsize=(20,15))
+    gudhi.plot_persistence_density(persistences, dimension=2)
+    plt.xlim([0,1]), plt.ylim([0,1])
+    plt.savefig(f'Plots/TDA/{population_name}_persistence_density_dim-2_{type}.png')
     plt.close()
 
 def load_matrix(path):
@@ -95,8 +154,25 @@ glioma_preop_paths = [path for path in files if '/Glioma/ses-preop/' in path]
 glioma_postop_paths = [path for path in files if '/Glioma/ses-postop/' in path]
 glioma_control_paths = [path for path in files if '/Glioma/ses-control/' in path]
 
-for subj in stroke_acute_paths:
-    test(drop_data_in_connect_matrix(load_matrix(subj)), subj.split(os.sep)[-1].split('.')[0])
+persistences = []
+procs = []
+threads = 6
+
+#perform_analysis(stroke_acute_paths, 'stroke_acute', True)
+#perform_analysis(stroke_control_paths, 'stroke_control', True)
+#perform_analysis(stroke_followup_paths, 'stroke_followup', True)
+#perform_analysis(stroke_followup_2_paths, 'stroke_followup_2', True)
+#perform_analysis(glioma_control_paths, 'glioma_control', True)
+#perform_analysis(glioma_postop_paths, 'glioma_postop', True)
+#perform_analysis(glioma_preop_paths, 'glioma_preop', True)
+
+#perform_analysis(stroke_acute_paths, 'stroke_acute', False)
+#perform_analysis(stroke_control_paths, 'stroke_control', False)
+#perform_analysis(stroke_followup_paths, 'stroke_followup', False)
+#perform_analysis(stroke_followup_2_paths, 'stroke_followup_2', False)
+#perform_analysis(glioma_control_paths, 'glioma_control', False)
+perform_analysis(glioma_postop_paths, 'glioma_postop', False)
+perform_analysis(glioma_preop_paths, 'glioma_preop', False)
 
 quit()
 for subj in stroke_control_paths:
